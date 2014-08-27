a545b53dd2724951f19ff364456e964d2bd59818
Mauricio Gamboa <mgamboa@mobiquityinc.com>
2014-08-27 14:56:37 -0600

    Are chart: yAxis to the right

--- public/scripts/areaChart.js
+++ public/scripts/areaChart.js
@@ -2,14 +2,22 @@ $(document).ready(function () {
   function areaChart(parent) {
 
     var _data = null;
-    var _capacity = null;
+    var _line = null;
+    _categories = null;
     
     function component () {
+
+      var theLabel = _data[0][0];
+      this.theAxe = {};
+      var someObject = "theAxe";
+      this[someObject][theLabel] = "y2";
+
       var chart = c3.generate({
         bindto: parent,
         data: {
           columns: _data,
-          type: 'area'
+          type: 'area',
+          axes: this.theAxe
         },
         point: { 
           show: false 
@@ -17,14 +25,23 @@ $(document).ready(function () {
         grid: {
           y: {
             show: true,
-            lines: [{value: _capacity, class: 'capacity-line'}]
+            lines: [{value: _line, class: 'capacity-line'}]
           }
         },
         axis: {
+          y: {
+            show: false
+          },
+          y2: {
+            show: true
+          },
           x: {
-            padding: {
-              left: 0,
-              right: 0 
+            type: 'category',
+            categories: _categories,
+            tick: {
+              culling: {
+                max: _categories.length
+              }
             }
           }
         },
@@ -45,21 +62,34 @@ $(document).ready(function () {
       return component;
     };
 
-    component.capacity = function(capacity) {
-      if (isNaN(capacity)) return;
-      _capacity = capacity;
+    component.categories = function(categories) {
+      if (!categories.length) return;
+      _categories = categories;
+      return component;
+    };
+
+    component.line = function(line) {
+      if (isNaN(line)) return;
+      _line = line;
       return component;
     };
 
     return component;
   }
 
-  var data1 =  ['Transient Rooms', 369, 376, 356, 334, 324, 303, 294, 277, 259, 243, 232, 224, 211, 199, 195, 179, 162, 151, 145, 132, 109, 106, 102, 99, 94, 93, 86, 81, 75, 70, 70, 66, 65, 59, 56, 56, 56, 54, 55, 52, 50, 49, 48, 49, 49, 49, 49, 48, 46, 46, 43, 42, 35, 27, 27, 25, 25, 26, 24, 23, 23, 23, 22, 22, 22, 20, 19, 18, 9, 8, 8, 5, 3, 3, 3];
-  var data2 = ['Group Room Nights', 94, 94, 94, 97, 98, 102, 102, 102, 106, 106, 109, 109, 109, 109, 109, 115, 116, 116, 116, 115, 115, 115, 114, 112, 111, 111, 109, 104, 104, 111, 111, 105, 106, 103, 98, 97, 95, 90, 87, 89, 87, 85, 84, 84, 73, 79, 79, 77, 77, 77, 76, 74, 70, 68, 67, 66, 64, 64, 64, 64, 69, 69, 69, 69, 68, 68, 62, 62, 58, 30, 30, 9, 9, 5, 0];
+  // These are the series to be shown in the chart, they must have the same length
+  var data1 = ['Label 1', 3, 3, 3, 5, 8, 8, 9, 18, 19, 20, 22, 22, 22, 23, 23, 23, 24, 26, 25, 25, 27, 27, 35, 42, 43, 46, 46, 48, 49, 49, 49, 49, 48, 49, 50, 52, 55, 54, 56, 56, 56, 59, 65, 66, 70, 70, 75, 81, 86, 93, 94, 99, 102, 106, 109, 132, 145, 151, 162, 179, 195, 199, 211, 224, 232, 243, 259, 277, 294, 303, 324, 334, 356, 376, 369];
+  var data2 = ['Label 2', 369, 376, 356, 334, 324, 303, 294, 277, 259, 243, 232, 224, 211, 199, 195, 179, 162, 151, 145, 132, 109, 106, 102, 99, 94, 93, 86, 81, 75, 70, 70, 66, 65, 59, 56, 56, 56, 54, 55, 52, 50, 49, 48, 49, 49, 49, 49, 48, 46, 46, 43, 42, 35, 27, 27, 25, 25, 26, 24, 23, 23, 23, 22, 22, 22, 20, 19, 18, 9, 8, 8, 5, 3, 3, 3];
+  
+  // Numbers or strings to show in the x axis, it must have the same lenth as the series
+  var theCategories =  [116, 109, 102, 95, 88, 81, 74, 67, 66, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 31, 30, 29, 28, 27, 26, 25, 24, 23, 22, 21, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0];
+  
+  // Add all the series to this array
   var theData = [data1, data2];
 
   var a1 = areaChart(document.getElementById('area-chart'))
-  .capacity(122)
   .data(theData)
+  .categories(theCategories)
+  .line(225)
   .render();
 });
